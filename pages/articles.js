92c4cc3c552354d916c0dcf2b760c4ae37e89e13
Daven Quinn <dev@davenquinn.com>
2019-10-08 16:28:53 -0500

    Updated article

--- pages/articles.js
+++ pages/articles.js
@@ -4,9 +4,12 @@ import BasePage from '../components/base-page'
 import dynamic from 'next/dynamic'
 import "@macrostrat/ui-components/lib/index.css"
 
-const GDDReferenceCard = dynamic(()=>
-  import('@macrostrat/ui-components').then(mod => mod.GDDReferenceCard)
-)
+const loadCard = async function(){
+  const mod = await import('@macrostrat/ui-components')
+  return mod.GDDReferenceCard
+}
+
+const GDDReferenceCard = dynamic(loadCard, { ssr: false });
 
 const Articles = () => {
   return <BasePage title="article search">
