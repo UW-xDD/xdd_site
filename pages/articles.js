4ba3a96f58e7aae90775461cdd28678e81485c73
Daven Quinn <dev@davenquinn.com>
2019-10-08 16:15:42 -0500

    Added API search

--- pages/articles.js
+++ pages/articles.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import Head from 'next/head'
 import BasePage from '../components/base-page'
+import dynamic from 'next/dynamic'
+import "@macrostrat/ui-components/lib/index.css"
+
+const GDDReferenceCard = dynamic(()=>
+  import('@macrostrat/ui-components').then(mod => mod.GDDReferenceCard)
+)
 
 const Articles = () => {
   return <BasePage title="article search">
     <p>An article</p>
+    <GDDReferenceCard docid="58b6b0dfcf58f110b18271ae" />
   </BasePage>
 }
 
