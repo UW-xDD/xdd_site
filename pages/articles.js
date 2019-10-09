f4e6e60604a83bab9588f4815e63217497c61719
Daven Quinn <dev@davenquinn.com>
2019-10-09 14:33:25 -0500

    Update state for docid search

--- pages/articles.js
+++ pages/articles.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Head from 'next/head'
 import BasePage from '../components/base-page'
 import dynamic from 'next/dynamic'
 import "@macrostrat/ui-components/lib/index.css"
 import "@blueprintjs/core/lib/css/blueprint.css"
-import {InputGroup} from "@blueprintjs/core"
+import {InputGroup, Callout} from "@blueprintjs/core"
 
 const loadCard = async function(){
   const mod = await import('@macrostrat/ui-components')
@@ -13,17 +13,47 @@ const loadCard = async function(){
 
 const GDDReferenceCard = dynamic(loadCard, { ssr: false });
 
-const Articles = () => {
-  return <BasePage title="article search">
-    <p>An article</p>
-    <InputGroup
-      className="main-search"
-      placeholder="Enter a docid"
-      leftIcon="search"
-      large
-    />
-    <GDDReferenceCard docid="58b6b0dfcf58f110b18271ae" />
-  </BasePage>
+const DocIDView = (props)=>{
+  const {searchString} = props;
+  let docid = null;
+  if (searchString.length == 24) {
+    docid = searchString;
+  }
+  if (docid != null) {
+    return <GDDReferenceCard docid={docid} />
+  }
+  return <Callout icon="alert" title="Invalid docid"
+    intent="warning">
+    A valid docid is 24 characters long!
+  </Callout>
+
+}
+
+class Articles extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchString: "58b6b0dfcf58f110b18271ae"
+    }
+    this.updateSearchString = this.updateSearchString.bind(this)
+  }
+  render() {
+    return <BasePage title="article search">
+      <InputGroup
+        className="main-search"
+        placeholder="Enter a docid"
+        leftIcon="search"
+        large
+        value={this.state.searchString}
+        onChange={this.updateSearchString}
+      />
+      <DocIDView searchString={this.state.searchString} />
+    </BasePage>
+  }
+
+  updateSearchString(evt) {
+    this.setState({searchString: evt.target.value})
+  }
 }
 
 export default Articles
