d18321c03418546e8a33d3883f0885a994a6fc18
Daven Quinn <dev@davenquinn.com>
2019-10-09 14:07:23 -0500

    Non-functional input box

--- pages/articles.js
+++ pages/articles.js
@@ -3,6 +3,8 @@ import Head from 'next/head'
 import BasePage from '../components/base-page'
 import dynamic from 'next/dynamic'
 import "@macrostrat/ui-components/lib/index.css"
+import "@blueprintjs/core/lib/css/blueprint.css"
+import {InputGroup} from "@blueprintjs/core"
 
 const loadCard = async function(){
   const mod = await import('@macrostrat/ui-components')
@@ -14,6 +16,12 @@ const GDDReferenceCard = dynamic(loadCard, { ssr: false });
 const Articles = () => {
   return <BasePage title="article search">
     <p>An article</p>
+    <InputGroup
+      className="main-search"
+      placeholder="Enter a docid"
+      leftIcon="search"
+      large
+    />
     <GDDReferenceCard docid="58b6b0dfcf58f110b18271ae" />
   </BasePage>
 }
