17c458b42b56871e429b778a0d626905c0646a44
Daven Quinn <dev@davenquinn.com>
2019-10-10 16:03:45 -0500

    Attempt to fix stylus but it doesn't really work

--- next.config.js
+++ next.config.js
@@ -1,4 +1,57 @@
 const withCSS = require('@zeit/next-css')
-//const withStylus = require('@zeit/next-stylus')
+const cssLoaderConfig = require('@zeit/next-css/css-loader-config')
 
-module.exports = withCSS()
+// Code from @zeit/next-stylus (hot-fixed)
+const withStylus = (nextConfig = {}) => {
+  return Object.assign({}, nextConfig, {
+    webpack(config, options) {
+      if (!options.defaultLoaders) {
+        throw new Error(
+          'This plugin is not compatible with Next.js versions below 5.0.0 https://err.sh/next-plugins/upgrade'
+        )
+      }
+
+      const { dev, isServer } = options
+      const {
+        cssModules,
+        cssLoaderOptions,
+        postcssLoaderOptions,
+        stylusLoaderOptions = {}
+      } = nextConfig
+
+      options.defaultLoaders.stylus = cssLoaderConfig(config, {
+        extensions: ['styl'],
+        cssModules,
+        cssLoaderOptions,
+        postcssLoaderOptions,
+        dev,
+        isServer,
+        loaders: [
+          {
+            loader: 'stylus-loader',
+            options: stylusLoaderOptions
+          }
+        ]
+      })
+
+      // HOTFIX
+      try {
+        delete options.defaultLoaders.stylus[2].options.minimize
+      } catch {}
+
+      config.module.rules.push({
+        test: /\.styl$/,
+        use: options.defaultLoaders.stylus
+      })
+
+      if (typeof nextConfig.webpack === 'function') {
+        return nextConfig.webpack(config, options)
+      }
+
+      return config
+    }
+  })
+}
+
+
+module.exports = withStylus(withCSS());
