879caa9923331bf80650c5b54e9dcd6a0b44e460
Daven Quinn <dev@davenquinn.com>
2020-09-22 13:57:32 -0500

    Basic snippets pages

--- pages/snippets.tsx
+++ pages/snippets.tsx
@@ -1,263 +1,297 @@
-import React, {createContext, useState, Component, useEffect} from 'react';
-import debounce from 'lodash.debounce';
-import Head from 'next/head'
-import BasePage from '../components/base-page'
-import dynamic from 'next/dynamic'
-import h from '@macrostrat/hyper'
-import "@macrostrat/ui-components/lib/esm/index.css"
-import "@blueprintjs/core/lib/css/blueprint.css"
-import { InputGroup, Callout, Button, Intent, Spinner } from "@blueprintjs/core"
-import { useSearchString } from '../components/search'
-import { LinkCard } from '../components/link-card'
-import { useRouter } from 'next/router';
+import React, { createContext, useState, Component, useEffect } from "react";
+import debounce from "lodash.debounce";
+import Head from "next/head";
+import BasePage from "../components/base-page";
+import dynamic from "next/dynamic";
+import h from "@macrostrat/hyper";
+import "@macrostrat/ui-components/lib/esm/index.css";
+import "@blueprintjs/core/lib/css/blueprint.css";
+import {
+  InputGroup,
+  Callout,
+  Button,
+  Intent,
+  Spinner,
+} from "@blueprintjs/core";
+import { useSearchString } from "../components/search";
+import { LinkCard } from "../components/link-card";
+import { useRouter } from "next/router";
 
 let ROUTE = "https://geodeepdive.org/api/snippets";
 
-const loadAPIResultView = async function(){
-    const mod = await import('@macrostrat/ui-components');
-    return mod.APIResultView
+/* These async functions create dynamically-loaded components that
+support next.js server-side rendering */
+
+const loadAPIResultView = async function () {
+  const mod = await import("@macrostrat/ui-components");
+  return mod.APIResultView;
 };
 const APIResultView = dynamic(loadAPIResultView, { ssr: false });
 
-const loadCard = async function(){
-    const mod = await import('@macrostrat/ui-components/lib/esm/infinite-scroll');
-    return mod.InfiniteScrollView
+const loadCard = async function () {
+  const mod = await import("@macrostrat/ui-components/lib/esm/infinite-scroll");
+  return mod.InfiniteScrollView;
 };
 const InfiniteScrollView = dynamic(loadCard, { ssr: false });
 
-const loadRefCard = async function(){
-    const mod = await import('@macrostrat/ui-components');
-    return mod.GddReferenceCard
+const loadRefCard = async function () {
+  const mod = await import("@macrostrat/ui-components");
+  return mod.GddReferenceCard;
 };
 const GddReferenceCard = dynamic(loadRefCard, { ssr: false });
 
-const loadRelatedTerms = async function(){
-    const mod = await import('@macrostrat/ui-components');
-    return mod.GeoDeepDiveRelatedTerms
+const loadRelatedTerms = async function () {
+  const mod = await import("@macrostrat/ui-components");
+  return mod.GeoDeepDiveRelatedTerms;
 };
 const GddRelatedTerms = dynamic(loadRelatedTerms, { ssr: false });
 
 const renderRelatedTerms = (res) => {
-    const related_terms = res.success;
-    return <GddRelatedTerms {...related_terms} />
+  const related_terms = res.success;
+  return <GddRelatedTerms {...related_terms} />;
 };
 
-const Highlight = ({highlight})=> {
-    return <li dangerouslySetInnerHTML={{__html : highlight}} />
+const Highlight = ({ highlight }) => {
+  return <li dangerouslySetInnerHTML={{ __html: highlight }} />;
 };
 
-const Highlights = ({highlights}) => {
-    if (highlights == null) return null;
-    return <ul>{highlights.slice(0,5).map( (highlight, index) => {
-        return <Highlight highlight={highlight} key={index} />
-    })}
+const Highlights = ({ highlights }) => {
+  if (highlights == null) return null;
+  return (
+    <ul>
+      {highlights.slice(0, 5).map((highlight, index) => {
+        return <Highlight highlight={highlight} key={index} />;
+      })}
     </ul>
+  );
 };
 
-const PaperResult = ({paper}) => {
-    return <LinkCard key={paper._gddid} className={`${paper._gddid} hit`} href={`/article/${paper._gddid}`}>
-        <h2>{paper.title}</h2>
-        <Highlights highlights={paper.highlight} />
+const PaperResult = ({ paper }) => {
+  return (
+    <LinkCard
+      key={paper._gddid}
+      className={`${paper._gddid} hit`}
+      href={`/article/${paper._gddid}`}
+    >
+      <h2>{paper.title}</h2>
+      <Highlights highlights={paper.highlight} />
     </LinkCard>
+  );
 };
 
 const PublicationSnippets = (props) => {
-    const {name, papers} = props;
-    const inner = papers.map( (paper, i) => h(PaperResult, {key: i, paper}));
+  const { name, papers } = props;
+  const inner = papers.map((paper, i) => h(PaperResult, { key: i, paper }));
 
-    return h("div.publication", [
-        h("h1.journal-title", name),
-        h("div", inner)
-    ])
+  return h("div.publication", [h("h1.journal-title", name), h("div", inner)]);
 };
 
-const SnippetResultsPage = (props)=>{
-    const {data} = props;
-    if (data == null || data.length == 0) return null;
+const SnippetResultsPage = (props) => {
+  const { data } = props;
+  if (data == null || data.length == 0) return null;
 
-    let results = {};
-    data.map ( (art) => {
-        if ( !(art["pubname"] in results) ) {
-            results[art['pubname']] = []
-        }
-        results[art['pubname']].push(art)
-    } );
-    const pubNames = Object.keys(results);
+  let results = {};
+  data.map((art) => {
+    if (!(art["pubname"] in results)) {
+      results[art["pubname"]] = [];
+    }
+    results[art["pubname"]].push(art);
+  });
+  const pubNames = Object.keys(results);
 
-    return <div className="snippets-page">
-        {pubNames.map((pub,i) => h(PublicationSnippets, {name: pub, papers: results[pub], key: i}))}
+  return (
+    <div className="snippets-page">
+      {pubNames.map((pub, i) =>
+        h(PublicationSnippets, { name: pub, papers: results[pub], key: i })
+      )}
     </div>
+  );
 };
-
 
 const SnippetResults = (props) => {
-    const { numberOfResults, data} = props;
+  const { numberOfResults, data } = props;
 
-    if (data == null || data.length == 0) return h('div.results', null, h(Spinner));
+  if (data == null || data.length == 0)
+    return h("div.results", null, h(Spinner));
 
-    let results = {};
-    data.map ( (art) => {
-        if ( !(art["pubname"] in results) ) {
-            results[art['pubname']] = []
-        }
-        results[art['pubname']].push(art)
-    } );
-
-    let countItem = null;
-    if (numberOfResults > 0) {
-        countItem = <div className="flex-end"><p className="count">{`${numberOfResults} articles found.`}</p></div>
+  let results = {};
+  data.map((art) => {
+    if (!(art["pubname"] in results)) {
+      results[art["pubname"]] = [];
     }
+    results[art["pubname"]].push(art);
+  });
 
-    return <div className='results'>
-        {countItem}
-        <div className="snippets">
-            {data.map((d,i) => h(SnippetResultsPage, {data: d, key: i}))}
-        </div>
+  let countItem = null;
+  if (numberOfResults > 0) {
+    countItem = (
+      <div className="flex-end">
+        <p className="count">{`${numberOfResults} articles found.`}</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="results">
+      {countItem}
+      <div className="snippets">
+        {data.map((d, i) => h(SnippetResultsPage, { data: d, key: i }))}
+      </div>
     </div>
+  );
 };
-
 
 const RelatedTermsView = (props) => {
-    const {searchString, debounce} = props;
+  const { searchString, debounce } = props;
 
-    if (searchString != null && searchString != '') {
-        return <APIResultView
-            route="https://geodeepdive.org/api/similar_terms"
-            params={{"term": searchString}}
-            debounce={debounce}>{renderRelatedTerms}</APIResultView>
-    }
-    return null
+  if (searchString != null && searchString != "") {
+    return (
+      <APIResultView
+        route="https://geodeepdive.org/api/similar_terms"
+        params={{ term: searchString }}
+        debounce={debounce}
+      >
+        {renderRelatedTerms}
+      </APIResultView>
+    );
+  }
+  return null;
 };
 
+const ResultView = (props) => {
+  const [data, setData] = useState({
+    params: { term: "", full_results: true, inclusive: true, article_limit: 2 },
+    success: null,
+    hits: 0,
+  });
+  // const [loading, setLoading] =  useState(false);
 
+  const { searchString } = props;
 
-const ResultView = (props)=> {
-    const [data, setData] = useState({
-        params: { term: '', full_results: true, inclusive: true, article_limit: 2 },
-        success: null,
-        hits: 0 }
-        );
-    // const [loading, setLoading] =  useState(false);
+  useEffect(() => {
+    setData({
+      params: { ...data.params, term: searchString },
+      success: null,
+      hits: 0,
+    });
 
-    const { searchString } = props;
+    // setLoading(true);
+  }, [searchString]);
 
+  if (searchString == null || searchString == "") {
+    return (
+      <Callout icon="search-text" title="Snippets" intent="primary">
+        Search xDD for contextual use of a term or phrase.
+      </Callout>
+    );
+  }
 
-    useEffect( () => {
-        setData({
-            params: {...data.params, term: searchString },
-            success: null,
-            hits: 0
-        });
+  function setDataState(response) {
+    if (response && response.success) {
+      // setLoading( false);
 
-        // setLoading(true);
-
-    }, [searchString]);
-
-
-    if(searchString == null || searchString == '') {
-        return <Callout icon="search-text" title="Snippets" intent="primary">
-            Search xDD for contextual use of a term or phrase.
-        </Callout>
+      setData({
+        ...data,
+        success: response.success,
+        hits: response.success.hits,
+      });
     }
+  }
 
-
-    function setDataState(response) {
-        if(response && response.success) {
-            // setLoading( false);
-
-            setData({
-                ...data,
-                success: response.success,
-                hits: response.success.hits
-            });
-        }
+  const handleResponse = (response) => {
+    if (response && response.success) {
+      setDataState(response);
+      return response.success;
     }
+  };
 
-    const handleResponse = (response) => {
-        if(response && response.success) {
-            setDataState(response);
-            return response.success;
-        }
-    };
+  const handleGetItems = (response) => {
+    if (response.success && response.success.data) {
+      setDataState(response);
+      return [response.success.data];
+    }
+  };
 
+  const handleGetNextParams = (response) => {
+    const { scrollId } = response?.success;
+    if (scrollId == null || scrollId == "") return null;
+    setDataState(response);
+    return { scroll_id: scrollId };
+  };
 
-    const handleGetItems = (response) => {
-        if(response.success && response.success.data) {
-            setDataState(response);
-            return [response.success.data];
-        }
-    };
+  const hasMore = (res) => {
+    const { scrollId } = res?.success;
+    // Override to short-circuit "load everything" bug
+    return false;
+    return scrollId != null && scrollId != "";
+  };
 
-
-    const handleGetNextParams = (response) => {
-        const { scrollId } = response?.success;
-        if (scrollId == null || scrollId == "") return null;
-        setDataState(response);
-        return { scroll_id: scrollId }
-    };
-
-
-    return <InfiniteScrollView
-        route={ ROUTE }
-        params={ data.params }
-        unwrapResponse={ handleResponse }
-        getNextParams={ handleGetNextParams }
-        getItems={ handleGetItems }
+  return (
+    <InfiniteScrollView
+      className={"results"}
+      route={ROUTE}
+      params={data.params}
+      unwrapResponse={handleResponse}
+      getNextParams={handleGetNextParams}
+      getItems={handleGetItems}
+      hasMore={hasMore}
     >
-        <SnippetResults numberOfResults={ data.hits } />
+      <SnippetResults numberOfResults={data.hits} />
     </InfiniteScrollView>
-
+  );
 };
-
 
 export default function SnippetsPage() {
-    const [inputValue, setInputValue] = useState("");
-    const [path, setPath] = useState("/snippets");
-    const [searchString, setSearchString] = useState("");
-    const router = useRouter();
+  const [inputValue, setInputValue] = useState("");
+  const [path, setPath] = useState("/snippets");
+  const [searchString, setSearchString] = useState("");
+  const router = useRouter();
 
-    useEffect( () => {
-        router.push(path, path, {shallow: true});
-    }, [path]);
+  useEffect(() => {
+    router.push(path, path, { shallow: true });
+  }, [path]);
 
-
-    function handleInputValueChange(e) {
-        if (e.target.value !== '') {
-            setPath('/snippets?search=' + e.target.value);
-        } else {
-            setPath("/snippets");
-        }
-
-        setInputValue(e.target.value);
+  function handleInputValueChange(e) {
+    if (e.target.value !== "") {
+      setPath("/snippets?search=" + e.target.value);
+    } else {
+      setPath("/snippets");
     }
 
-    function initiateSearch() {
-        setSearchString(inputValue);
-    }
+    setInputValue(e.target.value);
+  }
 
+  function initiateSearch() {
+    setSearchString(inputValue);
+  }
 
-    return<BasePage title="Snippets Search" fixedHeader={true}>
-        <div className="search-bar">
-            <InputGroup
-                className="main-search"
-                placeholder="Enter a search term"
-                leftIcon="search"
-                large
-                value={inputValue}
-                onChange={handleInputValueChange}
-                onKeyPress={event => {
-                    if (event.key === 'Enter') {
-                        initiateSearch()
-                    }
-                }}
-            />
+  return (
+    <BasePage title="Snippets Search" fixedHeader={true}>
+      <div className="search-bar">
+        <InputGroup
+          className="main-search"
+          placeholder="Enter a search term"
+          leftIcon="search"
+          large
+          value={inputValue}
+          onChange={handleInputValueChange}
+          onKeyPress={(event) => {
+            if (event.key === "Enter") {
+              initiateSearch();
+            }
+          }}
+        />
 
-            <Button icon='arrow-right' large onClick={() => {
-                initiateSearch()
-            }}/>
-        </div>
+        <Button
+          icon="arrow-right"
+          large
+          onClick={() => {
+            initiateSearch();
+          }}
+        />
+      </div>
 
-        <ResultView searchString={searchString}/>
-
+      <ResultView searchString={searchString} />
     </BasePage>
-};
+  );
+}
